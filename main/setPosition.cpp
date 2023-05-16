07fa6b318252e8af0aa21880f58ecae94eb7557a
humasoft <nicolecofl@unisabana.edu.co>
2023-05-16 13:35:37 +0200

    added LG0 value

--- main/setPosition.cpp
+++ main/setPosition.cpp
@@ -8,6 +8,9 @@
 #include "imu3dmgx510.h"
 #include "fcontrol.h"
 
+#define LG0 0.003 //meters
+#define radio 0.0075
+
 /* Programa test en modo posicion del cuello en bucle abierto */
 
 // pitch, roll to lenght of tendons in meters
@@ -29,24 +32,26 @@ int main (int argc, char *argv[])
     }
 
     std::chrono::system_clock::time_point start, current;
-    double radio = 0.0075; //meters
 
     //m1
     SocketCanPort pm1("can1");
     CiA402SetupData sd1(2048,24,0.001, 0.144, 10);
     CiA402Device m1 (1, &pm1, &sd1);
+    //m1.Reset();
     m1.SetupPositionMode(10, 10);
 
     //m2
     SocketCanPort pm2("can1");
     CiA402SetupData sd2(2048,24,0.001, 0.144, 10);
     CiA402Device m2 (2, &pm2, &sd2);
+    //m2.Reset();
     m2.SetupPositionMode(10, 10);
 
     //m3
     SocketCanPort pm3("can1");
     CiA402SetupData sd3(2048,24,0.001, 0.144, 10);
     CiA402Device m3 (3, &pm3, &sd3);
+    //m3.Reset();
     m3.SetupPositionMode(10, 10);
 
 
@@ -58,14 +63,16 @@ int main (int argc, char *argv[])
 
     start = std::chrono::system_clock::now();
 
+    printf("GetPosition -> %f %f %f\n", m1.GetPosition(), m2.GetPosition(), m3.GetPosition());
+
 
     printf("moving to pose: [%f] [%f]\n", pose[0], pose[1]);
 
     pr2tendons(pose[0], pose[1], lengths);
 
-    posan[0] = lengths[0]/radio;
-    posan[1] = lengths[1]/radio;
-    posan[2] = lengths[2]/radio;
+    posan[0] = (LG0-lengths[0])/radio;
+    posan[1] = (LG0-lengths[1])/radio;
+    posan[2] = (LG0-lengths[2])/radio;
 
     printf("tendons: [%f] [%f] [%f]\n", lengths[0], lengths[1], lengths[2]);
     printf("target angular pos : [%f] [%f] [%f]\n", posan[0], posan[1], posan[2]);
@@ -73,6 +80,8 @@ int main (int argc, char *argv[])
     m1.SetPosition(posan[0]);
     m2.SetPosition(posan[1]);
     m3.SetPosition(posan[2]);
+
+
 
     return 0;
 }
